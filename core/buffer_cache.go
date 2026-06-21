ab0db7ea04d124941460454d28fbeb825cac7b37
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-06-21 19:50:50 +0000

    fetch page from cache or disk

--- core/buffer_cache.go
+++ core/buffer_cache.go
@@ -1,5 +1,7 @@
 package core
 
+import "errors"
+
 const InvalidPageID = -1
 
 type Frame struct {
@@ -7,9 +9,74 @@ type Frame struct {
 	Dirty  bool
 	PinCnt uint32
 
-	Data [PageSize]byte
+	Page Page
 }
 
 type BufferPool struct {
-	Frames []Frame
+	Frames    []Frame
+	PageTable map[int]int
+
+	disk *DiskManager
+}
+
+func NewBufferPool(
+	size int,
+	disk *DiskManager,
+) *BufferPool {
+
+	frames := make([]Frame, size)
+
+	for i := range frames {
+		frames[i].PageID = InvalidPageID
+	}
+
+	return &BufferPool{
+		Frames: frames,
+
+		PageTable: make(map[int]int),
+
+		disk: disk,
+	}
+}
+
+func (bp *BufferPool) findFreeFrame() int {
+	for i := range bp.Frames {
+		if bp.Frames[i].PageID == InvalidPageID {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (bp *BufferPool) fetchPage(pageId int) (*Frame, error) {
+
+	if frameID, ok := bp.PageTable[pageId]; ok {
+		frame := &bp.Frames[frameID]
+		frame.PinCnt++
+
+		return frame, nil
+	}
+
+	frameID := bp.findFreeFrame()
+
+	if frameID == -1 {
+		return nil, errors.New("buffer pool full")
+	}
+
+	frame := &bp.Frames[frameID]
+
+	err := bp.disk.ReadPage(pageId, frame.Page.Data[:])
+
+	if err != nil {
+		return nil, err
+	}
+
+	frame.PageID = int32(pageId)
+	frame.PinCnt = 1
+	frame.Dirty = false
+
+	bp.PageTable[pageId] = frameID
+
+	return frame, nil
 }
